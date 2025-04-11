2d87a6cc13f324268700e0697f969c879e7dfb4d
udobaingo <160244098+udobaingo@users.noreply.github.com>
2025-04-11 20:09:11 -0300

    Update dia8.js
    Correcao Desafio 1

--- dia8.js
+++ dia8.js
@@ -59,19 +59,37 @@ console.log(`Saldo total do Banco 4: R$ ${saldoBanco4.toFixed(2)}`);
 
 // DESAFIO 1: Identifique 4 tipos diferentes de filas da vida real e escreva em topicos seu funcionamento com suas palavras
 
-// FILA 1: Fila no caixa do supermercado. O primeiro a chegar na fila passa primeiro as suas compras. Os proximos aguardam atras do 
-// primeiro, na sequencia, com seus carrinhos de compras. As compras do primeiro da fila sao colocadas sobre
-// uma esteira e o atendente escaneia seus codigos de barras ate concluir todos os itens que estavam no carrinho. Caixa diz o valor
-// e pergunta a forma de pagamento. Pagamento concluido. Depois, o proximo na fila coloca as compras na esteira e repete o procedimento.
+// FILA 1: Fila no caixa do supermercado. 
+// O primeiro a chegar na fila passa primeiro as suas compras. 
+// Os proximos aguardam atras do primeiro, na sequencia, com seus carrinhos de compras.
+// As compras do primeiro da fila sao colocadas sobre uma esteira
+// O atendente escaneia seus codigos de barras ate concluir todos os itens que estavam na esteira. 
+// Caixa diz o valor da compra e pergunta a forma de pagamento.
+// Pagamento concluido. 
+// Depois, o proximo na fila coloca as compras na esteira e repete o procedimento.
 
-// FILA 2: Fila no caixa eletronico. O primeiro da fila insere seu cartao ou faz verificacao por biometria para entrar no seu 
-// ambiente de atendimento. Escolhe o atendimento desejado, insere dados, se necessario, e conclui o atendimento recebendo um 
-// extrato, dinheiro ou outro documento comprobatorio
+// FILA 2: Fila no caixa eletronico. 
+// O primeiro da fila insere seu cartao ou faz verificacao por biometria para entrar no seu ambiente de atendimento. 
+// Escolhe o atendimento desejado, insere dados, se necessario, e conclui o atendimento recebendo um extrato, dinheiro ou
+// outro documento comprobatorio
 
-// FILA 3: Fila para atendimento na UPA. Primeiro atendimento, recebimento de uma senha. Aguardar a senha ser chamada para a triagem. 
-// Triagem com o atendente de triagem. Retorno ao guiche para coletar documentos e recebimento de uma senha nova. Aguardar nova senha
-// ser chamada pelo medico atendente. Atendimento pelo medico. Expedicao de receitas e demais documentos. Aguardar atendimento 
-// para atendimento pela enfermaria. Atendimento enfermaria. Saida da UPA com guia de liberacao assinado.
+// FILA 3: Fila para atendimento na UPA. 
+// Entrada 
+// Recebimento de uma senha a depender da idade e da gravidade 
+// Aguardar a senha ser chamada para a triagem. 
+// Triagem com o atendente de triagem. 
+// Retorno ao guiche para coletar documentos e recebimento de uma senha nova. 
+// Aguardar nova senha ser chamada pelo medico atendente. 
+// Atendimento pelo medico, expedicao de receitas e demais documentos. 
+// Aguardar atendimento pela enfermaria. 
+// Atendimento enfermaria. 
+// Saida da UPA com guia de liberacao assinado.
 
-// FILA 4: Fila na entrada de um jogo da Copa. Compra de tiquete online. Escaneamento de tiquete no computador local. 
-// Expedicao de documento para triagem. Triagem para a fila de entrada certa. Entrada na sequencia de entrada na fila de entrada certa.
+// FILA 4: Fila na entrada de um jogo da Copa. 
+// Compra de tiquete online. 
+// Escaneamento de tiquete no computador local. 
+// Expedicao de documento para triagem. 
+// Triagem para a fila de entrada certa. 
+// Entrada na sequencia de entrada na fila de entrada certa.
+// Escaneamento do codigo de barras na entrada do estadio.
+// 
