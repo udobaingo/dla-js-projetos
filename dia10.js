245f1cf5ff4a5417c4cc2e0b0dd57372a9bd1b50
udobaingo <udo.baingo@gmx.de>
2025-04-22 17:09:24 -0300

    Upload do Dia 10 com Desafio incluso

--- dia10.js
+++ dia10.js
@@ -25,6 +25,24 @@ jogarCarta();
 jogarCarta();
 jogarCarta();
 
-
-
 // Braço mecânico que empillha 10 produtos numa caixa e depois despacha essa caixa e disponibiliza outra caixa e outra etc.
+let pilhaDeProdutos = [];
+const produtosPorCaixa = 10;
+
+function empilharProdutos(produto) {
+    if(pilhaDeProdutos.length < produtosPorCaixa) {
+        pilhaDeProdutos.push(produto);
+        console.log(`Produto ${produto} empilhado.`);
+    } else {
+        console.log("Caixa cheia. Enviando para selagem e despacho.");
+        pilhaDeProdutos = [];
+        pilhaDeProdutos.push(produto);
+        console.log(`Produto ${produto} empilhado na nova caixa.`);
+    }
+}
+
+// Simulando a função com 40 produtos
+const TOTAL_DE_PRODUTOS_ESTEIRA = 40;
+for (let i = 1; i <= TOTAL_DE_PRODUTOS_ESTEIRA; i++) {
+    empilharProdutos(`${i}`);
+}
