f2c6122762d9a4ea8b22d45bf4fc95c5c18dc593
Udo Antônio Baingo <udo.baingo@gmx.de>
2025-05-17 00:28:02 -0300

    Test GCM authentication Bundalele

--- dia13.js
+++ dia13.js
@@ -77,85 +77,87 @@ console.log(`Último: ${ultimo()}`);
 // Implementar um deque para tarefas
 // Manipulação das prioridades
 // Testar com cenários realistas
-const tasks = [];
+        const tasks = [];
 
-// Insere task at the begin (highest priority)
-function insereBegin(task){
-    tasks.unshift(task);
-    console.log(`Task '${task}' added with highest priority`);
-}
+        // Insere task at the begin (highest priority)
+        function insereBegin(task){
+            tasks.unshift(task);
+            console.log(`Task '${task}' added with highest priority`);
+        }
 
-// Insere task to the end (lowest priority)
-function insereEnd(task){
-    tasks.push(task);
-    console.log(`Task '${task}' added with lowest priority`);
-}
+        // Insere task to the end (lowest priority)
+        function insereEnd(task){
+            tasks.push(task);
+            console.log(`Task '${task}' added with lowest priority`);
+        }
 
-// Remove task from the begin (highest priority)
-function removeBegin(){
-    if (!isEmpty()){
-        console.log(`Task '${tasks.shift()}' with highest priority has been removed.`);
-    } else {
-    console.log("There are no tasks to be removed.");
-    }
-}
+        // Remove task from the begin (highest priority)
+        function removeBegin(){
+            if (!isEmpty()){
+                console.log(`Task '${tasks.shift()}' with highest priority has been removed.`);
+            } else {
+            console.log("There are no tasks to be removed.");
+            }
+        }
 
-function removeEnd(){
-    if (!isEmpty()){
-    console.log(`Task '${tasks.pop()}' with lowest priority has been removed.`);
-    } else {
-    console.log("There are no tasks to be removed.");
-    }
-}
+        function removeEnd(){
+            if (!isEmpty()){
+            console.log(`Task '${tasks.pop()}' with lowest priority has been removed.`);
+            } else {
+            console.log("There are no tasks to be removed.");
+            }
+        }
 
-function isEmpty(){
-    return tasks.length === 0;
-}
+        function isEmpty(){
+            return tasks.length === 0;
+        }
 
-function getTasks(){
-    return tasks.slice();
-}
+        function getTasks(){
+            return tasks.slice();
+        }
 
-// Quero priorizar algo que está numa posição intermediária, colocando-a uma acima
-// Quero despriorizar algo que está numa posição qualquer, colocando-a uma abaixo
-// Por exemplo, posição 7 para posição 6 ou 9 para 8
+        // Quero priorizar algo que está numa posição intermediária, colocando-a uma acima
+        // Quero despriorizar algo que está numa posição qualquer, colocando-a uma abaixo
+        // Por exemplo, posição 7 para posição 6 ou 9 para 8
 
-function setHigherPriority(task) {
-    let index = tasks.indexOf(task);
-    if (index > 0) {
-    let temp = tasks[index-1];
-    tasks[index - 1] = tasks[index];
-    tasks[index] = temp;
-    console.log(`Priority of task '${task}' has been set higher.`);
-    } else {
-     console.log("The task has already the highest priority or does not exist");
-    }
-}
+        function setHigherPriority(task) {
+            let index = tasks.indexOf(task);
+            if (index > 0) {
+            let temp = tasks[index-1];
+            tasks[index - 1] = tasks[index];
+            tasks[index] = temp;
+            console.log(`Priority of task '${task}' has been set higher.`);
+            } else {
+            console.log("The task has already the highest priority or does not exist");
+            }
+        }
 
-function setLowerPriority(task) {
-    let index = tasks.indexOf(task);
-    if (index !== -1 &&  index < tasks.length - 1) {
-        let temp = tasks[index + 1];
-        tasks[index + 1] = tasks[index];
-        tasks[index] = temp;
-        console.log(`Priority of task '${task}' has been set lower.`);
-        } else {
-         console.log("The task has alredy the lowest priority or does not exist");
-    }
-}
+        function setLowerPriority(task) {
+            let index = tasks.indexOf(task);
+            if (index !== -1 &&  index < tasks.length - 1) {
+                let temp = tasks[index + 1];
+                tasks[index + 1] = tasks[index];
+                tasks[index] = temp;
+                console.log(`Priority of task '${task}' has been set lower.`);
+                } else {
+                console.log("The task has alredy the lowest priority or does not exist");
+            }
+        }
 
-insereEnd("Buy Coffee");
-insereBegin("Reply E-Mails");
-insereEnd("Meeting");
+        insereEnd("Buy Coffee");
+        insereBegin("Reply E-Mails");
+        insereEnd("Meeting");
 
-console.log("Actual tasks: " , getTasks());
+        console.log("Actual tasks: " , getTasks());
 
-setHigherPriority("Buy Coffee");
+        setHigherPriority("Buy Coffee");
 
-setLowerPriority("Reply E-Mails");
+        setLowerPriority("Reply E-Mails");
 
-console.log("Tasks after reorganization: ", getTasks());
+        console.log("Tasks after reorganization: ", getTasks());
 
-removeBegin();
+        removeBegin();
 
-console.log("Tasks after removing:", getTasks());
+        console.log("Tasks after removing:", getTasks());
+
+        // Bundalele
